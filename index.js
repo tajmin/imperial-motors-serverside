5aab515d9eda040492b3274f2694d95a38af7116
tajmin <tajminur.aiub@gmail.com>
2021-11-12 02:54:59 +0600

    order api

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
-
 const app = express();
 const port = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -31,12 +31,27 @@ async function run() {
             res.send(result);
         });
 
+        //Get: Car by Id
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await carCollection.findOne(query);
+            res.json(result);
+        });
+
         // POST: Save User
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
             res.json(result);
         })
+
+        //Post: New Order
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
 
     } finally {
 
