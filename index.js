df4d206f03c659ad0161bde32add71058317e20a
tajmin <tajminur.aiub@gmail.com>
2021-11-11 11:15:15 +0600

    get cars

--- index.js
+++ index.js
@@ -12,8 +12,37 @@ app.use(cors());
 app.use(express.json());
 
 //Mongodb Connect
-const uri = "mongodb+srv://<username>:<password>@cluster0.7xlcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('imperial');
+        const userCollection = database.collection('users');
+        const orderCollection = database.collection('orders');
+        const carCollection = database.collection('cars');
+        console.log('db connected');
+
+        //GET: Get Cars
+        app.get('/cars', async (req, res) => {
+            const cursor = carCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // POST: Save User
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.json(result);
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Ah, here We Go!')
